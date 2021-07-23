6f8cafe9ed71124632b5b0dd93d3eb926c38c63d
Dustyinc <dimitriosantoulis@googlemail.com>
2021-07-23 15:07:53 +0200

    Current version update

---
+++ tf_beach_tileA1.tsx
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="tf_beach_tileA1" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="tf_beach_tileA1.png" width="512" height="384"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <animation>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="53" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="56">
+  <animation>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="61" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="96">
+  <animation>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="100" duration="100"/>
+   <frame tileid="101" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="104">
+  <animation>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="109" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="144">
+  <animation>
+   <frame tileid="144" duration="100"/>
+   <frame tileid="145" duration="100"/>
+   <frame tileid="146" duration="100"/>
+   <frame tileid="147" duration="100"/>
+   <frame tileid="148" duration="100"/>
+   <frame tileid="149" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="100"/>
+   <frame tileid="153" duration="100"/>
+   <frame tileid="154" duration="100"/>
+   <frame tileid="155" duration="100"/>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="157" duration="100"/>
+  </animation>
+ </tile>
+ <wangsets>
+  <wangset name="Water" type="edge" tile="6">
+   <wangcolor name="" color="#ffaaff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="6" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="8" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="48" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="56" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="96" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="104" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="144" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="152" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ tf_beach_tileB.tsx
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="tf_beach_tileB" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="tf_beach_tileB.png" width="512" height="512"/>
+ <tile id="1" probability="0.05"/>
+ <tile id="2" probability="0.05"/>
+ <tile id="3" probability="0.05"/>
+ <tile id="4" probability="0.05"/>
+ <tile id="5" probability="0.05"/>
+ <tile id="6" probability="0.05"/>
+ <tile id="7" probability="0.75"/>
+ <tile id="16" probability="0.005"/>
+ <tile id="17" probability="0.005"/>
+ <tile id="18" probability="0.005"/>
+ <tile id="19" probability="0.005"/>
+ <tile id="20" probability="0.005"/>
+ <tile id="21" probability="0.005"/>
+ <tile id="22" probability="0.005"/>
+ <tile id="23" probability="0.005"/>
+ <tile id="32" probability="0.005"/>
+ <tile id="33" probability="0.005"/>
+ <tile id="34" probability="0.005"/>
+ <wangsets>
+  <wangset name="BeachSand" type="corner" tile="7">
+   <wangcolor name="" color="#e7e70a" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="7" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="22" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="23" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="33" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="152" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="153" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="154" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="155" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="156" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="157" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="158" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="168" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="171" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="172" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="174" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="184" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="185" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="186" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="187" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="188" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="190" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="204" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="205" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="206" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+  <wangset name="WetSand" type="corner" tile="169">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="152" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="153" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="154" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="155" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="156" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="157" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="158" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="168" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="169" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="171" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="172" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="174" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="184" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="185" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="186" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="187" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="188" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="190" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="204" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="205" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="206" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
