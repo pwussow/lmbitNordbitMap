13c9f82ae0deb7c0f7e9cd4b63b4a3f3592bb5a1
Patrick Wussow <patrick.wussow@lmbit.de>
2021-07-28 09:32:36 +0200

    some changes

--- Begrenzung_Steinmauer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Begrenzung_Steinmauer" tilewidth="32" tileheight="32" tilecount="30" columns="5">
- <image source="Tilepictures/Begrenzungen/Begrenzung_Steinmauer.png" trans="ffffff" width="160" height="192"/>
-</tileset>
